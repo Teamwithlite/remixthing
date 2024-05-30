98c6974a8d75eba226d0d8fd7a7e5e63342243a9
Teamwithlite <chanakan.k@beryl8.com>
2024-05-31 01:46:33 +0700

    card

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,13 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function Index() {
   return (
@@ -10,6 +18,36 @@ export default function Index() {
       <div className="flex space-x-3 px-4">
         <Button>Submit</Button>
         <Input />
+      </div>
+      <div className="flex flex-col space-y-4 w-auto px-4 py-4">
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </div>
   );
